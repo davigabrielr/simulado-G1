4ca7449b4fd306158c846df8661a5b1349867f8f
davigabrielr <davi.roza@amf.edu.br>
2026-06-22 14:30:57 -0300

    Correct syntax and logic errors in exercicio1.py
    Fix syntax errors and improve function definitions.

---
+++ exercicio1.py
@@ -0,0 +1,44 @@
+def multliplo4(numero) 
+  while numero % 4 != 0:
+    numero -= 1
+  returne(numero)
+
+def menu():
+  print("1 - somar")
+  print("2 - subtrair")
+  print("3 - mostrar valores")
+  print("4 - sair ")
+  
+v1 = int(input(""))
+v2 = int(input(""))
+
+opcao = 0
+
+def somar (a, b):
+  print("resultado:", a + b):
+
+def subtracao (a, b):
+  print("resultado:", a - b):
+
+def apresentar_valor (a, b):
+  prit("valor 1:" , a)
+  print("valor 2:" , b)
+
+while opcao != 4:
+  opcao = menu()
+
+if opcao == 1
+  somar (v1, v2)
+
+elif opcao == 2:
+  subtrair (v1, v2)
+
+elif opcao == 3:
+ apresentar_valor (v1, v2)
+retar
+elif opcao == 4;
+  print("programa encerrado ")
+
+else:
+  print("erro)
+
